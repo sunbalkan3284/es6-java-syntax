58e058cdaedf5e32a360c88e125e2cfed1b03a57
sunbalkan3284 <33255561+sunbalkan3284@users.noreply.github.com>
2022-05-02 11:04:29 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * スプレッド構文
+ */
+
+//配列の展開
+const arr1 = [1, 2];
+// console.log(arr1);
+
+// console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
